fcbc14ca98ff4d15d8a209d8cf64860d276d5686
Baby Indra Nandagopal <nnandagopal@yahoo.com>
2022-09-22 01:59:08 -0400

    Updated README with PNG files

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Belly-Button-Biodiversity
+
+In this project, an interactive dashboard was built to explore the [Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonize human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+## Creation of Dashboard
+
+Completed the following steps:
+
+1. Used the D3 library to read in `samples.json`
+
+2. Created a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+  * Used `sample_values` as the values for the bar chart.
+  * Used `otu_ids` as the labels for the bar chart.
+  * Used `otu_labels` as the hovertext for the chart.
+
+  ![bar Chart](Images/bar_chart.png)
+
+3. Created a bubble chart that displays each sample.
+
+  * Used `otu_ids` for the x values.
+  * Used `sample_values` for the y values.
+  * Used `sample_values` for the marker size.
+  * Used `otu_ids` for the marker colors.
+  * Used `otu_labels` for the text values.
+
+![Bubble Chart](Images/bubble_chart.png)
+
+4. Displayed the sample metadata, i.e., an individual's demographic information.
+
+5. Displayed each key-value pair from the metadata JSON object on the page.
+
+![Demographic Info](Images/Demographic_Info.png)
+
+6. Updated all the plots when a new sample is selected.
+
+Here is the Belly Button BioDiversity Dashboard created: 
+![Belly Button BioDiversity Dashboard](Images/Belly_Button_Biodiversity_Dashboard.png)
+
+
+## Advanced Challenge (Optional)
+
+* Adapted the Gauge Chart from <https://plot.ly/javascript/gauge-charts/> to plot the weekly washing frequency of the individual.
+
+* Updated the gauge code to account for values ranging from 0 through 9.
+
+* Updated the chart whenever a new sample is selected.
+
+![Weekly Washing Frequency Gauge](Images/gauge_chart.png)
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-//Build function to read json file using d3
+//Build function to read json file using d3 and plot charts
 function buildPlots(id) {
     
     d3.json("../../samples.json").then((data) => {
@@ -20,27 +20,27 @@ function buildPlots(id) {
         // Create variables for bar chart & bubble chart
         // get the sample_values for the top 10 OTU ids for the bar chart
         var sample_values = filteredSample.sample_values
-        var Top10_sample_values = filteredSample.sample_values.slice(0, 10).reverse()
         console.log(sample_values);
+        var Top10_sample_values = filteredSample.sample_values.slice(0, 10).reverse()
+        console.log(Top10_sample_values);
 
         // Use otu_ids as the labels for bar chart & bubble chart
         var otu_ids = filteredSample.otu_ids
-        var Top10_otu_ids = filteredSample.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse()
-        
         console.log(otu_ids);
-
+        var Top10_otu_ids = filteredSample.otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse()
+        console.log(Top10_otu_ids);
+        
         // use otu_labels as the hovertext for bar chart & bubble chart
         var otu_labels = filteredSample.otu_labels
         console.log(otu_labels);
 
         // Create the trace for Bar chart
         var bar_trace = [{
-            // Use otu_ids for the x values
-            
+            // Use sample_values for the x values
             x: Top10_sample_values,
-            // Use sample_values for the y values
+            // Use otu_ids for the y values
             y: Top10_otu_ids,
-            // Use otu ids
+            
             text: otu_labels.slice(0, 10).reverse(),
             type: 'bar',
             orientation: 'h',
@@ -79,15 +79,16 @@ function buildPlots(id) {
             height: 600,
             width: 1000
         };
-        
+
         Plotly.newPlot("bubble", bubble_trace, bubble_layout);
 
-        // GAUGE CHART
-        // Create variable for washing frequency
+     //****************** BONUS Optional*****************//
+
+        // GAUGE CHART - Create variable for washing frequency
         var washFreq = filteredMetadata.wfreq
 
         // Create the trace
-        var gauge_data = [
+        var gauge_trace = [
             {
                 domain: { x: [0, 1], y: [0, 1] },
                 value: washFreq,
@@ -95,25 +96,24 @@ function buildPlots(id) {
                 type: "indicator",
                 mode: "gauge+number",
                 gauge: {
-                    bar: {color: 'white'},
-                    axis: { range: [null, 9] },
+                    bar: {color: 'maroon'},
+                    axis: { range: [null, 9],
+                        tickvals: [0,1,2,3,4,5,6,7,8,9],
+                        ticks: "outside" },
                     steps: [
-                        { range: [0, 3], color: 'rgb(253, 162, 73)' },
-                        { range: [3, 6], color: 'rgb(242, 113, 102)' },
-                        { range: [6, 9], color: 'rgb(166, 77, 104)' },
+                        { range: [0, 3], color: 'rgb(207, 236, 207)' },
+                        { range: [3, 6], color: 'rgb(181, 225, 174)' },
+                        { range: [6, 9], color: 'rgb(145, 210, 144)' },
                     ],
-                    // threshold: {
-                    //     line: { color: "white" },
-                    // }
                 }
             }
         ];
 
-        // Define Plot layout
+        // Define guage layout
         var gauge_layout = { width: 500, height: 400, margin: { t: 0, b: 0 } };
 
         // Display plot
-        Plotly.newPlot('gauge', gauge_data, gauge_layout);
+        Plotly.newPlot('gauge', gauge_trace, gauge_layout);
     });
 };
 
@@ -145,8 +145,8 @@ function optionChanged(id) {
     DemographicInfo(id);
 }
 
-//function to initial setting up of Dashboard
-function init_Dashboard() {
+//function to show the Dashboard
+function show_Dashboard() {
     var dropdown = d3.select("#selDataset")
     d3.json("samples.json").then(data => {
         
@@ -155,10 +155,12 @@ function init_Dashboard() {
         id.forEach(id => {
             dropdown.append("option").text(id).property("value", id)
         });
+
+        //show charts and demographic info by calling the resp. functions
         buildPlots(id[0]);
         DemographicInfo(id[0]);
     });
 };
 
-//Calling init_Dashboard function
-init_Dashboard();
+//Calling show_Dashboard function
+show_Dashboard();
